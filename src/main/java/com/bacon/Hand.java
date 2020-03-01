7d628ada1c470dfd18b85bb472adac2f0a35779a
David Bacon <david@baconsoft.io>
2020-03-01 13:48:18 +0000

    Determine one pair hand

--- src/main/java/com/bacon/Hand.java
+++ src/main/java/com/bacon/Hand.java
@@ -1,15 +1,58 @@
 package com.bacon;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static com.bacon.HandName.HIGH_CARD;
+import static com.bacon.HandName.ONE_PAIR;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class Hand {
     private HandName name;
 
-    public Hand(String cards) {
-        name = HIGH_CARD;
+    public Hand(String cardDefinitions) {
+        name = parseDefinitions(cardDefinitions);
+    }
+
+    private HandName parseDefinitions(String cardDefinitions) {
+        List<Card> cards = Stream.of(cardDefinitions.split(" ")).map(Card::new).collect(Collectors.toList());
+        assert (cards.size() == 5);
+
+        Map<Character, Long> rankCount =
+                cards.stream().collect(groupingBy(Card::getRank, counting()));
+
+       if (rankCount.containsValue(2L)) {
+           return ONE_PAIR;
+       } else {
+           return HIGH_CARD;
+       }
+
     }
 
     HandName getName() {
         return name;
+    }
+
+    private static class Card {
+        private char rank;
+        private char suit;
+
+        public Card(String definition) {
+            assert (definition.length() == 2);
+            // further card validation could be carried out here
+            this.rank = definition.charAt(0);
+            this.suit = definition.charAt(1);
+        }
+
+        public char getRank() {
+            return rank;
+        }
+
+        public char getSuit() {
+            return suit;
+        }
     }
 }
